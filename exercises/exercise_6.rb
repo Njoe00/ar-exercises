a37be0f4f6291c37bd49e01672f4d178c6bb2e3d
Njoe00 <nick.james.joe@gmail.com>
2023-01-19 10:07:15 +0000

    submitting question 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,11 +8,11 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store1.employees.create(first_name: "John", last_name: "Doe", hourly: 15)
-@store2.employees.create(first_name: "Daniel", last_name: "Crag", hourly: 25)
-@store2.employees.create(first_name: "Ash", last_name: "Ketchum", hourly: 5)
-@store1.employees.create(first_name: "Team", last_name: "Rocket", hourly: 100)
-@store2.employees.create(first_name: "Bob", last_name: "Dylan", hourly: 10000)
+@Store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@Store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 15)
+@Store2.employees.create(first_name: "Daniel", last_name: "Crag", hourly_rate: 25)
+@Store2.employees.create(first_name: "Ash", last_name: "Ketchum", hourly_rate: 5)
+@Store1.employees.create(first_name: "Team", last_name: "Rocket", hourly_rate: 100)
+@Store2.employees.create(first_name: "Bob", last_name: "Dylan", hourly_rate: 10000)
 
 
