626ab786c2a83dece1489f2eb3f598a1a8da3479
Njoe00 <nick.james.joe@gmail.com>
2023-01-19 07:28:46 +0000

    adding completion of first exercise

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,24 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+Store.create(
+    name: "Burnaby",
+    womens_apparel:  true,
+    mens_apparel:  true,
+    annual_revenue: 3000000
+)
+Store.create(
+    name: "Richmond",
+    womens_apparel: true,
+    mens_apparel: false,
+    annual_revenue: 1260000
+)
+Store.create(
+    name: "Gastown",
+    mens_apparel: true,
+    womens_apparel: false,
+    annual_revenue: 190000
+)
+
+
+puts Store.count
